8b36d2ccd21c3762fcacb1629e6019e59a08fdb6
1MatthewHarris1 <1MatthewHarris1@gmail.com>
2018-11-15 12:48:30 -0800

    Delete GlobalData.py

--- GlobalData.py
+++
@@ -1,4 +0,0 @@
-DATABASE_FILENAME = './RDC.sqlite3'
-valid_image_extensions = ['png', 'jpg', 'jpeg', 'tiff', 'bmp']
-downloaded_image_folder_location = './Downloaded'
-finished_image_folder_location = './Images'
